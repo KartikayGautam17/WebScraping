86ddc0e70ee7edd2da9137c4294ec7550c78faa3
kartikay <kartikaygautam24617@gmail.com>
2024-04-24 09:20:42 +0530

    1

--- src/scraper/results.js
+++ src/scraper/results.js
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 import jQuery from "jquery";
+import fs from "fs";
 
 const page_url = "https://www.amazon.in/";
 
@@ -11,11 +12,8 @@ async function Scrape() {
   const page = await browser.newPage();
   await page.goto(page_url, { waitUntil: "domcontentloaded" });
   const data = await page.content();
-  //const target = jQuery.parseHTML(data);
-  //console.log(target);
-  const results = [];
-
-  console.log(results);
-  console.log(jQuery().jquery);
+  console.log(jQuery());
+  await browser.close();
+  //console.log(jQuery().jquery);
 }
 Scrape();
